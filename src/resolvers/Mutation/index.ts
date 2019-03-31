7595a6f17abc474e9d4cf5022d53714cf2209a13
João Inez <joaoinez@Joaos-MacBook-Pro.local>
2019-03-31 17:21:27 +0100

    Adds encryption to user id and password

--- src/resolvers/Mutation/index.ts
+++ src/resolvers/Mutation/index.ts
@@ -1,4 +1,5 @@
 import { getConnection } from 'typeorm'
+import bcrypt = require('bcrypt')
 import { User } from '../../entity/User'
 
 const Mutation = {
@@ -11,10 +12,11 @@ const Mutation = {
         password
       }: { username: string; email: string; password: string }
     ) => {
+      const hashedPassword = await bcrypt.hash(password, 10)
       const user = new User()
       user.username = username
       user.email = email
-      user.password = password
+      user.password = hashedPassword
 
       const userRepository = getConnection().getRepository(User)
 
